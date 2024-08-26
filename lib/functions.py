041f0826065ecd6b7bed790da974fb057272479a
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-08-26 14:48:37 +0300

    lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
-def greet(name):
-    pass
+def greet(Geoffrey):
+    print(f"Hello, {Geoffrey}!")
 
 def greet_with_default(name="programmer"):
-    pass
-
+    print(f"Hello, {name}!")
+    return name
 def add(num1, num2):
-    pass
+    num1 = 45
+    num2 = 55
+    return num1 + num2
 
-def halve(number):
-    pass
+def halve(num):
+    return num/2
